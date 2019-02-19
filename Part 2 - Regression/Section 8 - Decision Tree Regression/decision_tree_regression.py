2d0777a1fbffab54b6c254a22875da7b30c84919
tany1610 <tany130@abv.bg>
2019-02-19 10:36:24 +0200

    complete desicion tree regression section

---
+++ Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression.py
@@ -0,0 +1,43 @@
+# Decision Tree Regression
+
+# Polynomial Regression Template
+
+# Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Loading the dataset
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:, 1:2].values
+Y = dataset.iloc[:, 2].values
+
+# Split the data into training set and test set
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)"""
+
+# feature scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X  = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
+
+# Fitting the Regression Modelto the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X, Y)
+
+# Predicting new data
+y_pred = regressor.predict([[6.5]])
+
+
+# Visualizing the Polynomial Regression
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, Y, color="red")
+plt.plot(X_grid, regressor.predict(X_grid), color="blue")
+plt.title("Salary by Level (Polynomial Regression)")
+plt.xlabel("Level")
+plt.ylabel("Salary")
+plt.show()
