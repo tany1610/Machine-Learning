eed4417532c396402d780b86b1e4bba53fbd7e02
tany1610 <tany130@abv.bg>
2019-02-20 09:44:16 +0200

    complete random forest regression set

--- Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
+++ Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
@@ -1,6 +1,4 @@
-# Decision Tree Regression
-
-# Polynomial Regression Template
+# Random Forest Regression
 
 # Importing libraries
 import numpy as np
@@ -24,20 +22,20 @@ X_test = sc_X.transform(X_test)"""
 
 
 # Fitting the Regression Model to the dataset
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state=0)
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(random_state=0, n_estimators=300)
 regressor.fit(X, Y)
 
 # Predicting new data
 y_pred = regressor.predict([[6.5]])
 
 
-# Visualizing the Decision Tree Regression
+# Visualizing the Random Forest Regression
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, Y, color="red")
 plt.plot(X_grid, regressor.predict(X_grid), color="blue")
-plt.title("Salary by Level (Decision Tree Regression)")
+plt.title("Salary by Level (Random Forest Regression)")
 plt.xlabel("Level")
 plt.ylabel("Salary")
 plt.show()
